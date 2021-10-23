d43937eca0995b305a23b72f547ca02e936483e1
Luis Herrera <luhegoes@gmail.com>
2021-10-23 08:01:13 -0700

    Setup Java project

---
+++ src/main/java/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+import java.util.logging.Logger;
+
+public class App {
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("Practica calificada 01");
+    }
+}
---
+++ src/test/java/app/AppTest.java
@@ -0,0 +1,14 @@
+package app;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+    @Test
+    public void main(){
+        String[] args = new String[1];
+        args[0] = "Practica Calificada 01";
+        App.main(args);
+        Assert.assertTrue(true);
+    }
+}
